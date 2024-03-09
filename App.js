b86f55c389b80ffc50916c1b5a42c9e213e87366
usama786nazeer <162768348+usama786nazeer@users.noreply.github.com>
2024-03-09 12:47:30 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,69 @@
+//chapter no 1 alert
+
+
+ // Greet the visitor with an alert box
+// alert("Welcome to our website! We hope you enjoy your visit.");
+// Display a message in an alert box
+// alert("JavaScript Alert\n\nError! Please enter a valid password.");
+// alert("Welcome to JS Land!\nHappy Coding!")
+
+ 
+// chapter no 2 variables for string
+
+// var usama;
+// var myName = "usama nazeer";
+// var message;
+// message = "Hello World";
+// alert(message);
+// var name = "Usama";
+// var age = 19;
+
+// // Show name in an alert box
+// alert("Name: " + name);
+
+// // Show age in another alert box
+// alert("Age: " + age);
+
+// var email = "example@example.com";
+// alert("My email address is: " + "usama@gmail.com");
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I am currently reading the book: " + book);
+// document.write("yah! i can write HTML content through JavaScript");
+
+
+// var myString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(myString);
+// document.write(myString);
+
+//chapter no 3 variables for numbers
+
+// var age = 18; 
+
+// // Show your age in an alert box
+// alert("My age is: " + age);
+
+// // Check if the visitor has visited the page before by checking if there's a value stored in localStorage
+// var visits = localStorage.getItem('visits');
+// visits = visits ? parseInt(visits) + 1 : 1;
+
+// // Store the updated value back in localStorage
+// localStorage.setItem('visits', visits);
+
+// // Display a message on the webpage showing the number of visits
+// alert("You have visited this site " + visits + " times.");
+
+
+// var visitorName = "John Doe";
+// var productTitle = "T-shirt";
+// var quantity = 5;
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+
+//chapter no 4 LEGAL & ILLEGAL 
+
+// // Display the information in the browser
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("<p><b>a)</b> Variable names can only contain letters, digits, underscores, and dollar signs. For example $my_1stVariable.</p>");
+// document.write("<p><b>b)</b> Variables must begin with a letter, underscore, or dollar sign. For example $name, _name, or name.</p>");
+// document.write("<p><b>c)</b> Variable names are case sensitive.</p>");
+// document.write("<p><b>d)</b> Variable names should not be JS reserved keywords.</p>");
